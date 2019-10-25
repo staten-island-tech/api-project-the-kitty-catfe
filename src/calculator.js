556e96ba121ac96f5c5111363f8a4619d2089423
WhalenSITHS <38401651+WhalenSITHS@users.noreply.github.com>
2019-10-25 11:44:18 -0400

    Initial commit

---
+++ src/calculator.js
@@ -0,0 +1,13 @@
+export default class Caclulator{
+    add(a, b) {
+      return a + b;
+    };
+  
+    multiply(a, b) {
+      return a * b;
+    };
+  
+    divide(a, b) {
+      return a / b;
+    };
+  } ;
