6dc3b0d56c07fd9df10c8b2e2226b3810632d194
EdwardNg-Tech <55099020+EdwardNg-Tech@users.noreply.github.com>
2019-11-09 11:47:01 -0500

    Update fetchAPI.js

--- src/scripts/fetchAPI.js
+++ src/scripts/fetchAPI.js
@@ -3,7 +3,7 @@ const fetchAPI = {
   getLocation() {
     DOMStrings.userInputClick.addEventListener('click', async function(event) {
       event.preventDefault();
-      const city = DOMStrings.userInputData;
+      const city = DOMStrings.userInputData.value;
       console.log(city);
     });
   }
