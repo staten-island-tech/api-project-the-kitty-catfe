115037eef85bdf9ccac5e9574621ed824a594215
EdwardNg-Tech <55099020+EdwardNg-Tech@users.noreply.github.com>
2019-11-10 09:14:00 -0500

    Update dataConstruction.js

--- src/scripts/dataConstruction.js
+++ src/scripts/dataConstruction.js
@@ -8,16 +8,21 @@ class Forecast {
     probabilityOfPercipitation,
     humidity,
     windDirection,
-    windSpeed
+    windSpeed,
+    sunrise,
+    sunset
   ) {
-    (this.temperature = temperature),
-      (this.temperatureMaximum = temperatureMaximum),
-      (this.temperatureMinimum = temperatureMinimum),
-      (this.weatherHourly = weatherHourly),
-      (this.weatherDaily = weatherDaily),
-      (this.probabilityOfPercipitation = probabilityOfPercipitation),
-      (this.humidity = humidity),
-      (this.windDirection = windDirection),
-      (this.windSpeed = windSpeed);
+    this.temperature = temperature;
+    this.temperatureMaximum = temperatureMaximum;
+    this.temperatureMinimum = temperatureMinimum;
+    this.weatherHourly = weatherHourly;
+    this.weatherDaily = weatherDaily;
+    this.probabilityOfPercipitation = probabilityOfPercipitation;
+    this.humidity = humidity;
+    this.windDirection = windDirection;
+    this.windSpeed = windSpeed;
+    this.sunrise = sunrise;
+    this.sunset = sunset;
   }
 }
+function dataConstruction() {}
