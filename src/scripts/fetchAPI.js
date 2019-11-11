96a7a254b26c43ba44d11c64e6d2e4e8e0b7ec50
EdwardNg-Tech <55099020+EdwardNg-Tech@users.noreply.github.com>
2019-11-10 22:24:25 -0500

    Revert "Constructs temperature array."
    This reverts commit c0ebe4c2863d13dda83592973b4557d2b8794204.

--- src/scripts/fetchAPI.js
+++ src/scripts/fetchAPI.js
@@ -38,13 +38,13 @@ const fetchAPI = {
     forecastHourlyLink,
     forecastGridLink
   ) {
-    const currentTimeData = new Date();
+    const currentDate = new Date();
     try {
       let forecastData = fetch(forecastLink);
       let forecastHourlyData = fetch(forecastHourlyLink);
       let forecastGridData = fetch(forecastGridLink);
       let sunriseSunset = fetch(
-        `https://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}&date=${currentTimeData.getFullYear()}-${currentTimeData.getMonth()}-${currentTimeData.getDate()}&formatted=0`
+        `https://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}&date=${currentDate.getFullYear()}-${currentDate.getMonth()}-${currentDate.getDate()}&formatted=0`
       );
       const returnArray = await Promise.all([
         forecastData,
@@ -59,11 +59,13 @@ const fetchAPI = {
         forecastGridData,
         sunriseSunset
       ] = await Promise.all(returnData);
+
       forecastData = forecastData.properties;
       forecastHourlyData = forecastHourlyData.properties;
       forecastGridData = forecastGridData.properties;
       const sunrise = sunriseSunset.results.sunrise;
       const sunset = sunriseSunset.results.sunset;
+      console.log(sunrise);
     } catch {}
   }
 };
